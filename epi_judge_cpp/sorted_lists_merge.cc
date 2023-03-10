49eac26d5154fe406d1f997308cf730a85b2e814
Vlad Matulis <matulis@pm.me>
2023-03-10 22:18:29 +0100

    solve sorted lists merge

--- epi_judge_cpp/sorted_lists_merge.cc
+++ epi_judge_cpp/sorted_lists_merge.cc
@@ -1,9 +1,31 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
-shared_ptr<ListNode<int>> MergeTwoSortedLists(shared_ptr<ListNode<int>> L1,
-                                              shared_ptr<ListNode<int>> L2) {
-  // TODO - you fill in here.
-  return nullptr;
+
+#include <memory>
+
+namespace {
+  void AppendNode(
+    shared_ptr<ListNode<int>> *where,
+    shared_ptr<ListNode<int>> *what
+  ) {
+    (*where)->next = *what;
+    *where = (*where)->next;
+    *what = (*what)->next;
+  }
+}
+
+shared_ptr<ListNode<int>> MergeTwoSortedLists(
+  shared_ptr<ListNode<int>> L1,
+  shared_ptr<ListNode<int>> L2
+) {
+  auto dummy_head = std::make_shared<ListNode<int>>();
+  auto tail = dummy_head;
+
+  while (L1 && L2)
+    AppendNode(&tail, L1->data < L2->data ? &L1 : &L2);
+  tail->next = L1 ? L1 : L2;
+
+  return dummy_head->next;
 }
 
 int main(int argc, char* argv[]) {
