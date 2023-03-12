ff219c424b2051700730c8d85d6d50145dde8e6e
Vlad Matulis <matulis@pm.me>
2023-03-12 19:17:36 +0100

    solve is tree balanced

--- epi_judge_cpp/is_tree_balanced.cc
+++ epi_judge_cpp/is_tree_balanced.cc
@@ -1,8 +1,53 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
-bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+
+#include <iostream>
+
+// #define log(x) std::cout << #x << ": " << (x) << std::endl
+#define log(x) nullptr
+
+using node_ptr = unique_ptr<BinaryTreeNode<int>>;
+
+namespace {
+  int height(const node_ptr& tree) {
+    return tree ? std::max(height(tree->left), height(tree->right)) + 1 : 0;
+  }
+
+  // Passing
+  bool trivial(const node_ptr& tree) {
+    if (!tree) return true;
+
+    const auto left_height = height(tree->left);
+    const auto right_height = height(tree->right);
+    const auto balanced = std::abs(left_height - right_height) <= 1;
+
+    return balanced ? trivial(tree->left) && trivial(tree->right) : false;
+  }
+
+  // The height is valid only if balanced
+  struct TraverseData {
+    int height;
+    bool balanced;
+  };
+
+  const TraverseData optimal(const node_ptr& tree) {
+    if (!tree) return { 0, true };
+
+    const auto left_data = optimal(tree->left);
+    if (!left_data.balanced) return left_data;
+
+    const auto right_data = optimal(tree->right);
+    if (!right_data.balanced) return right_data;
+
+    const auto height = std::max(left_data.height, right_data.height) + 1;
+    const auto balanced = std::abs(left_data.height - right_data.height) <= 1;
+    return { height, balanced };
+  }
+}
+
+bool IsBalanced(const node_ptr& tree) {
+  // return trivial(tree);
+  return optimal(tree).balanced;
 }
 
 int main(int argc, char* argv[]) {
