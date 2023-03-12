526379c11305b6f9e40b5ff01c33d0ee40205c0f
Vlad Matulis <matulis@pm.me>
2023-03-12 21:49:11 +0100

    solve sorted arrays merge

--- epi_judge_cpp/sorted_arrays_merge.cc
+++ epi_judge_cpp/sorted_arrays_merge.cc
@@ -1,10 +1,43 @@
+#include <queue>
 #include <vector>
 
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::priority_queue;
+
+namespace {
+  struct HeapData {
+    vector<int>::const_iterator i;
+    vector<int>::const_iterator to;
+  };
+
+  bool operator>(const HeapData& l, const HeapData& r) {
+    return *l.i > *r.i;
+  }
+}
+
+// It works two times faster if used with hand-implemented heap
+// But I guess the cost is that this faster implementation is less production-ready
 vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
-  // TODO - you fill in here.
-  return {};
+  vector<int> res;
+  priority_queue<HeapData, vector<HeapData>, std::greater<HeapData>> heap;
+
+  for (size_t i = 0; i < sorted_arrays.size(); ++i) {
+    if (sorted_arrays[i].empty()) continue;
+
+    heap.push({ sorted_arrays[i].begin(), sorted_arrays[i].end() - 1});
+  }
+
+  while (!heap.empty()) {
+    const auto top = heap.top();
+    res.emplace_back(*top.i);
+    heap.pop();
+
+    if (top.i != top.to)
+      heap.push({ top.i + 1, top.to });
+  }
+
+  return res;
 }
 
 int main(int argc, char* argv[]) {
