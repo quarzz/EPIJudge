294a7dcef3ed10cc7aba07ff0424003dcaadea9a
Vlad Matulis <matulis@pm.me>
2023-03-13 22:09:33 +0100

    solve intersect sorted arrays

--- epi_judge_cpp/intersect_sorted_arrays.cc
+++ epi_judge_cpp/intersect_sorted_arrays.cc
@@ -3,10 +3,73 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-vector<int> IntersectTwoSortedArrays(const vector<int>& A,
-                                     const vector<int>& B) {
-  // TODO - you fill in here.
-  return {};
+namespace {
+  // Passing
+  vector<int> impl_no_if_empty(const vector<int>& A, const vector<int>& B) {
+    vector<int> res;
+    auto i_a = A.cbegin();
+    auto i_b = B.cbegin();
+
+    while (i_a != A.cend() && i_b != B.cend()) {
+      if (*i_a < *i_b) {
+        ++i_a;
+      } else if (*i_a > *i_b) {
+        ++i_b;
+      } else {
+        res.emplace_back(*i_a);
+        ++i_a;
+        ++i_b;
+        break;
+      }
+    }
+
+    while (i_a != A.cend() && i_b != B.cend()) {
+      if (*i_a < *i_b) {
+        ++i_a;
+      } else if (*i_a > *i_b) {
+        ++i_b;
+      } else {
+        if (res.back() != *i_a)
+          res.emplace_back(*i_a);
+
+        ++i_a;
+        ++i_b;
+      }
+    }
+
+    return res;
+  }
+
+  // Passing
+  // I expected to notice decrease in performance for additional if check
+  //   but I guess branch prediction unit is too good nowadays
+  vector<int> impl_if_empty(const vector<int>& A, const vector<int>& B) {
+    vector<int> res;
+    auto i_a = A.cbegin();
+    auto i_b = B.cbegin();
+
+    while (i_a != A.cend() && i_b != B.cend()) {
+      if (*i_a < *i_b) {
+        ++i_a;
+      } else if (*i_a > *i_b) {
+        ++i_b;
+      } else {
+        if (res.empty() || res.back() != *i_a)
+          res.emplace_back(*i_a);
+        ++i_a;
+        ++i_b;
+      }
+    }
+
+    return res;
+  }
+};
+
+vector<int> IntersectTwoSortedArrays(
+  const vector<int>& A,
+  const vector<int>& B
+) {
+  return impl_no_if_empty(A, B);
 }
 
 int main(int argc, char* argv[]) {
