3cdb72b11d144456ee17734c6e73a2662a4e3699
Vlad Matulis <matulis@pm.me>
2023-03-20 17:59:01 +0100

    git solve task pairing

--- epi_judge_cpp/task_pairing.cc
+++ epi_judge_cpp/task_pairing.cc
@@ -4,15 +4,46 @@
 #include "test_framework/fmt_print.h"
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
+
 using std::vector;
 
 struct PairedTasks {
   int task_1, task_2;
 };
 
+namespace {
+  // Passing
+  vector<PairedTasks> greedy(vector<int>& task_durations) {
+    vector<PairedTasks> res;
+
+    const auto n = task_durations.size();
+    std::sort(task_durations.begin(), task_durations.end());
+    for (size_t i = 0; i < n / 2; ++i)
+      res.push_back({task_durations[i], task_durations[n - 1 - i]});
+
+    return res;
+  }
+
+  // Passing
+  vector<PairedTasks> cache_friendly(vector<int>& task_durations) {
+    const auto n = task_durations.size();
+    const auto k = n / 2;
+    vector<PairedTasks> res { k };
+
+    std::sort(task_durations.begin(), task_durations.end());
+
+    for (size_t i = 0; i < n / 2; ++i)
+      res[i].task_1 = task_durations[i];
+
+    for (size_t i = 0; i < n / 2; ++i)
+      res[k - 1 - i].task_2 = task_durations[n / 2 + i];
+
+    return res;
+  }
+}
+
 vector<PairedTasks> OptimumTaskAssignment(vector<int> task_durations) {
-  // TODO - you fill in here.
-  return {};
+  return cache_friendly(task_durations);
 }
 
 namespace test_framework {
