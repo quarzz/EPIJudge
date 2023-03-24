061bd7d234e5edcf9ca9559a809d292ef2c1a3ed
Vlad Matulis <matulis@pm.me>
2023-03-24 23:55:32 +0100

    solve spiral ordering

--- epi_judge_cpp/spiral_ordering.cc
+++ epi_judge_cpp/spiral_ordering.cc
@@ -2,9 +2,34 @@
 
 #include "test_framework/generic_test.h"
 using std::vector;
+
+namespace {
+  // Passing
+  vector<int> layer_by_layer(const vector<vector<int>>& A) {
+    const size_t n = A.size();
+    vector<int> res;
+    res.reserve(n * n);
+
+    for (size_t i = 0; i < n / 2; ++i) {
+      for (size_t j = i; j < n - 1 - i; ++j)
+        res.push_back(A[i][j]);
+      for (size_t j = i; j < n - 1 - i; ++j)
+        res.push_back(A[j][n - 1 - i]);
+      for (size_t j = i; j < n - 1 - i; ++j)
+        res.push_back(A[n - 1 - i][n - 1 - j]);
+      for (size_t j = i; j < n - 1 - i; ++j)
+        res.push_back(A[n - 1 - j][i]);
+    }
+
+    if (n % 2)
+      res.push_back(A[n / 2][n / 2]);
+
+    return res;
+  }
+}
+
 vector<int> MatrixInSpiralOrder(const vector<vector<int>>& square_matrix) {
-  // TODO - you fill in here.
-  return {};
+  return layer_by_layer(square_matrix);
 }
 
 int main(int argc, char* argv[]) {
