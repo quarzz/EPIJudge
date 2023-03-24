46358c9798854f75a8c40ea0c0f6ff00f4808409
Vlad Matulis <matulis@pm.me>
2023-03-24 15:16:24 +0100

    solve power x y

--- epi_judge_cpp/power_x_y.cc
+++ epi_judge_cpp/power_x_y.cc
@@ -1,7 +1,86 @@
 #include "test_framework/generic_test.h"
+
+namespace {
+  double naive_positive(double x, int y);
+
+  // Passing
+  //   I guess this one is more precise, because of denormalized doubles
+  //   But because of these denormals it can be slower for negative powers
+  double naive_with_denorms(double x, int y) {
+    return y < 0 ? naive_positive(1.0 / x, -y) : naive_positive(x, y);
+  }
+
+  // Passing
+  //   This one, for example, is a bit faster
+  //   But I am not sure whether it is as precise. Need to check later.
+  // TODO: read the line above
+  double naive_without_denorms(double x, int y) {
+    return y < 0 ? 1.0 / naive_positive(x, -y) : naive_positive(x, y);
+  }
+
+  // Passing
+  // Slowest
+  double naive_iffed(double x, int y) {
+    double res = 1.0;
+
+    while (y) {
+      if (y > 0) {
+        res *= x;
+        --y;
+      } else {
+        res /= x;
+        ++y;
+      }
+    }
+
+    return res;
+  }
+
+  double naive_positive(double x, int y) {
+    double res = 1.0;
+    while (y--) res *= x;
+    return res;
+  }
+
+  double log_positive(double x, int y);
+
+  // Passing
+  double log_with_denorms(double x, int y) {
+    return y < 0 ? log_positive(1.0 / x, -y) : log_positive(x, y);
+  }
+
+  double ad_hoc(double x, int y) {
+    return y < 0 ? log_positive(1 / x, -y) : log_positive(x, y);
+  }
+
+  // Passing
+  double log_without_denorms(double x, int y) {
+    return y < 0 ? 1.0 / log_positive(x, -y) : log_positive(x, y);
+  }
+
+  double log_positive(double x, int y) {
+    // Invariant: res = s * x ^ y
+    auto s = 1.0;
+
+    while (y) {
+      if (y % 2) {
+        s *= x;
+        --y;
+      } else {
+        x *= x;
+        y /= 2;
+      }
+    }
+
+    return s;
+  }
+
+  // P.S. The difference in denorms/no denorms vanished after changing / to *
+}
+
 double Power(double x, int y) {
   // TODO - you fill in here.
-  return 0.0;
+  return log_with_denorms(x, y);
 }
 
 int main(int argc, char* argv[]) {
