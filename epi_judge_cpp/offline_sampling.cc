7c91c3ac03f41cff40b11775eb33c1c7d688189d
Vlad Matulis <matulis@pm.me>
2023-03-24 22:15:51 +0100

    solve offline sampling

--- epi_judge_cpp/offline_sampling.cc
+++ epi_judge_cpp/offline_sampling.cc
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <functional>
 #include <iterator>
+#include <random>
 #include <vector>
 
 #include "test_framework/generic_test.h"
@@ -8,9 +9,43 @@
 #include "test_framework/timed_executor.h"
 using std::bind;
 using std::vector;
+
+#define log(x) std::cout << #x << ": " << (x) << std::endl
+
+namespace {
+  void multiple_distrib(int k, vector<int>* A_ptr) {
+    auto& A = *A_ptr;
+    std::random_device rd;
+    std::mt19937 gen { rd() };
+
+    const size_t n = A.size();
+    // log(k); log(n);
+    for (size_t i = 0; i < k; ++i) {
+      std::uniform_int_distribution<> distrib(i, n - 1);
+      std::swap(A[i], A[distrib(gen)]);
+    }
+
+    A.erase(A.cbegin() + k, A.cend());
+  }
+
+  void one_distrib(int k, vector<int>* A_ptr) {
+    auto& A = *A_ptr;
+    const size_t n = A.size();
+    std::random_device rd;
+    std::mt19937 gen { rd() };
+    std::uniform_int_distribution<> distrib(0, n - 1);
+
+    for (size_t i = 0; i < k; ++i) {
+      const auto j = n - 1 - distrib(gen) % (n - i);
+      std::swap(A[i], A[j]);
+    }
+
+    A.erase(A.cbegin() + k, A.cend());
+  }
+}
+
 void RandomSampling(int k, vector<int>* A_ptr) {
-  // TODO - you fill in here.
-  return;
+  one_distrib(k, A_ptr);
 }
 bool RandomSamplingRunner(TimedExecutor& executor, int k, vector<int> A) {
   using namespace test_framework;
