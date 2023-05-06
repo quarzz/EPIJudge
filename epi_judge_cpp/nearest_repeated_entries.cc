ea0479c490c8930e12744b6456e491920f89ee4b
Vlad Matulis <matulis@pm.me>
2023-05-06 20:44:53 +0300

    solve nearest repeated entries

--- epi_judge_cpp/nearest_repeated_entries.cc
+++ epi_judge_cpp/nearest_repeated_entries.cc
@@ -5,9 +5,28 @@
 using std::string;
 using std::vector;
 
+namespace {
+
+  int trivial_with_hash(const std::vector<std::string>& paragraph) {
+    std::unordered_map<std::string, decltype(paragraph.begin())> word_to_prev_i;
+    ptrdiff_t min_distance = std::numeric_limits<ptrdiff_t>::max();
+
+    for (auto i = paragraph.begin(); i != paragraph.end(); ++i) {
+      if (auto it = word_to_prev_i.find(*i); it != word_to_prev_i.end()) {
+        min_distance = std::min(min_distance, i - it->second);
+        it->second = i;
+      }
+
+      word_to_prev_i.emplace(*i, i);
+    }
+
+    return min_distance;
+  }
+}
+
 int FindNearestRepetition(const vector<string>& paragraph) {
   // TODO - you fill in here.
-  return 0;
+  return trivial_with_hash(paragraph);
 }
 
 int main(int argc, char* argv[]) {
