a4ac0a4156481a352a1cbaf36a97045ba6ca87ac
Vlad Matulis <uladzislau.matulis@mapbox.com>
2025-03-30 11:36:09 +0300

    solve 6.4 is string palindromic

--- epi_judge_cpp/is_string_palindromic.cc
+++ epi_judge_cpp/is_string_palindromic.cc
@@ -3,7 +3,11 @@
 #include "test_framework/generic_test.h"
 using std::string;
 bool IsPalindromic(const string& s) {
-  // TODO - you fill in here.
+  for (size_t i = 0; i < s.size() / 2; ++i) {
+    if (s[i] != s[s.size() - 1 - i]) {
+      return false;
+    }
+  }
   return true;
 }
 
